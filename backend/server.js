2325eeac6a7eb5048beae6b04e6d2d89e6923f94
Alifdanish74 <alifdanish74@gmail.com>
2024-11-17 18:06:54 +0800

    test vercel FE

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:5173", "https://jemput-nikah-av8w.vercel.app/"],
+    origin: ["http://localhost:5173", "https://jemput-nikah-av8w.vercel.app"],
   })
 );
 
