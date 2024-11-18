dbe0a9a0a10e7cab963646a82384688f5bae5e73
Alifdanish74 <alifdanish74@gmail.com>
2024-11-18 23:08:12 +0800

    hellooo helopop

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", 
-  build: {
-    outDir: "dist", // Ensure your output directory is 'dist' (default for Vite)
-  },
   server: {
-    port: 4000, // Your local development server port
-  },
-  define: {
-    "process.env": {}, // Helps mimic Node.js `process.env` in Vite
+    port: 5173, // Vite dev server port
+    proxy: {
+      "/api": {
+        target: "http://localhost:4000", // Backend server
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 });
