516e16b53bc035c6a7bf5f553841f0bd48a2d71f
Alifdanish74 <alifdanish74@gmail.com>
2024-11-19 11:34:08 +0800

    hehe

--- backend/routes/weddingCard.js
+++ backend/routes/weddingCard.js
@@ -50,7 +50,7 @@ router.get("/download-image/:id", async (req, res) => {
   try {
     // Step 1: Fetch the presigned URL from S3
     const presignedUrlResponse = await axios.get(
-      `${import.meta.env.VITE_API_BASE_URL}/api/wedding-cards/${id}/presigned-url`
+      `https://jemputkahwin-backend.onrender.com/api/wedding-cards/${id}/presigned-url`
     );
     const { url } = presignedUrlResponse.data;
 
